e5c5c47ff8228a28732b1f9ffc26f0f5ebc7eb09
Maxim Martynov <max.mrtnv@gmail.com>
2022-05-03 15:17:19 +0400

    Removed updating theme with JS

--- storefront.js
+++ storefront.js
@@ -1,13 +1,3 @@
 console.log("TELEGRAM WEB APP:", window.Telegram.WebApp);
 
-function applyTelegramTheme() {
-  const { themeParams } = window.Telegram.WebApp;
-
-  document.body.style.background = themeParams.bg_color;
-}
-
-applyTelegramTheme();
-
-window.Telegram.WebApp.onEvent("themeChanged", function () {
-  applyTelegramTheme();
-});
+window.Telegram.WebApp.onEvent("themeChanged", function () {});
